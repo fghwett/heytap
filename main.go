30f2c6984b47c52bfd8437c8d0390e79f9348dd3
fghwett <1058178245@qq.com>
2021-10-12 20:08:37 +0800

    读取配置文件

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/fghwett/heytap/config"
+)
+
+var path = flag.String("path", "./config.yml", "配置文件地址")
+
 func main() {
+	flag.Parse()
+
+	_, err := config.Init(*path)
+	if err != nil {
+		fmt.Printf("读取配置文件失败 err: %s", err)
+		os.Exit(-1)
+	}
 
 }
